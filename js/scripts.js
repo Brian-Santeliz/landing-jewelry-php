3ac22049dc316438710c32ee4213b4dd95b5c365
Brian <blsvsanteliz@gmail.com>
2021-03-06 18:02:26 -0400

    add date with js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
-(function() {
-     'use strict';
-     window.addEventListener('load', function() {
-       // Seleccionar el formulario para validar
-       var forms = document.getElementsByClassName('needs-validation');
-       // Validar cada campo y prevenir que se envie
-       var validation = Array.prototype.filter.call(forms, function(form) {
-         form.addEventListener('submit', function(event) {
-           if (form.checkValidity() === false) {
-             event.preventDefault();
-             event.stopPropagation();
-           }
-           form.classList.add('was-validated');
-         }, false);
-       });
-     }, false);
-   })();
+(function () {
+  window.addEventListener(
+    "load",
+    function () {
+      const forms = document.getElementsByClassName("needs-validation");
+      const footer = document.getElementById("footer");
+      const fecha = new Date().getFullYear();
+      footer.innerHTML = `${fecha}`;
+      const validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
