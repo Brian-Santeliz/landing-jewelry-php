dbbf378f6c5f8887ff8b4e2a492395c43b11d6a5
Brian <blsvsanteliz@gmail.com>
2021-03-09 18:10:57 -0400

    add header in templates

---
+++ templates/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <title>Nova Joyeria | Joyas de otro mundo</title>
+    <link href="css/css.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/styles.css" />
+  </head>
+  <body>
+    <header class="encabezado-sitio container">
+      <div class="row justify-content-md-between align-items-center">
+        <div class="col-lg-4">
+          <a href="index.php"> Nova Joyeria </a>
+        </div>
+        <div class="col-lg-4">
+          <nav class="sociales text-center text-lg-right">
+            <ul>
+              <li>
+                <a href="http://facebook.com">
+                  <span class="sr-only"> Facebook</span>
+                </a>
+              </li>
+              <li>
+                <a href="http://twitter.com">
+                  <span class="sr-only"> Twitter</span>
+                </a>
+              </li>
+              <li>
+                <a href="http://instagram.com">
+                  <span class="sr-only"> Instagram</span>
+                </a>
+              </li>
+              <li>
+                <a href="http://pinterest.com">
+                  <span class="sr-only"> Pinterest</span>
+                </a>
+              </li>
+              <li>
+                <a href="http://youtube.com">
+                  <span class="sr-only"> YouTube</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
