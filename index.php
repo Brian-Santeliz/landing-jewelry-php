2f11f3fa5070e6353ceb6d68524f57c804cd6501
Brian <blsvsanteliz@gmail.com>
2021-03-09 18:09:42 -0400

    rename all the files to php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <header class="encabezado-sitio container">
       <div class="row justify-content-md-between align-items-center">
         <div class="col-lg-4">
-          <a href="index.html"> Nova Joyeria </a>
+          <a href="index.php"> Nova Joyeria </a>
         </div>
         <div class="col-lg-4">
           <nav class="sociales text-center text-lg-right">
@@ -69,19 +69,19 @@
           <div class="collapse navbar-collapse w-100" id="nav_principal">
             <ul class="nav nav-justified w-100 flex-column flex-sm-row">
               <li class="nav-item">
-                <a href="index.html" class="nav-link">Inicio</a>
+                <a href="index.php" class="nav-link">Inicio</a>
               </li>
               <li class="nav-item">
-                <a href="nosotros.html" class="nav-link">Nosotros</a>
+                <a href="nosotros.php" class="nav-link">Nosotros</a>
               </li>
               <li class="nav-item">
-                <a href="servicios.html" class="nav-link">Servicios</a>
+                <a href="servicios.php" class="nav-link">Servicios</a>
               </li>
               <li class="nav-item">
-                <a href="productos.html" class="nav-link">Productos</a>
+                <a href="productos.php" class="nav-link">Productos</a>
               </li>
               <li class="nav-item">
-                <a href="contacto.html" class="nav-link">Contacto</a>
+                <a href="contacto.php" class="nav-link">Contacto</a>
               </li>
             </ul>
           </div>
@@ -170,7 +170,7 @@
                   nosotros
                 </h2>
                 <a
-                  href="nosotros.html"
+                  href="nosotros.php"
                   class="btn btn-primary btn-block text-uppercase mt-4 py-3"
                   >Leer más</a
                 >
@@ -188,7 +188,7 @@
                   servicios
                 </h2>
                 <a
-                  href="servicios.html"
+                  href="servicios.php"
                   class="btn btn-primary btn-block text-uppercase mt-4 py-3"
                   >Leer más</a
                 >
@@ -206,7 +206,7 @@
                   productos
                 </h2>
                 <a
-                  href="productos.html"
+                  href="productos.php"
                   class="btn btn-primary btn-block text-uppercase mt-4 py-3"
                   >Leer más</a
                 >
@@ -359,79 +359,11 @@
             tus disponibilidad 8 sucursales para que escojas la que más cerca
             que este de ti. ¡Ven y visítanos! Te esperamos en Nova Joyería.
           </p>
-          <a href="contacto.html" class="btn btn-primary mt-3 text-uppercase"
+          <a href="contacto.php" class="btn btn-primary mt-3 text-uppercase"
             >Leer más</a
           >
         </div>
       </div>
     </div>
 
-    <footer class="footer-sitio pt-3 mt-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <h3 class="text-uppercase text-center pb-4">Nosotros</h3>
-            <p class="text-justify">
-              Somos una empresa que nos caracterizamos por nuestras prendas de
-              alta calidad. Contamos con una alta gama de joyas que van desde
-              anillos de propuesta hasta pequeños broches.
-            </p>
-          </div>
-          <div class="col-md-4 text-center">
-            <h3 class="text-uppercase pb-4">Paginas</h3>
-            <a class="nav-link btn-link text-dark" href="index.html">Inicio</a>
-            <a class="nav-link btn-link text-dark" href="servicios.html"
-              >Servicios</a
-            >
-            <a class="nav-link btn-link text-dark" href="contacto.html"
-              >Contácto</a
-            >
-          </div>
-          <div class="col-md-4 text-center">
-            <h3 class="text-uppercase pb-4">Visitános</h3>
-            <p>Local pricipal: Valera, CC Edivica</p>
-            <p>Planta baja, local 37</p>
-            <nav class="sociales text-center">
-              <ul>
-                <li>
-                  <a href="http://facebook.com">
-                    <span class="sr-only"> Facebook</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="http://twitter.com">
-                    <span class="sr-only"> Twitter</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="http://instagram.com">
-                    <span class="sr-only"> Instagram</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="http://pinterest.com">
-                    <span class="sr-only"> Pinterest</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="http://youtube.com">
-                    <span class="sr-only"> YouTube</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <hr class="w-100" />
-          <p class="text-center copyright w-100">
-            Nova Joyeria <span id="footer"></span>. Todos los derechos
-            reservados &copysr;
-          </p>
-        </div>
-      </div>
-    </footer>
-    <script src="js/jquery.slim.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/scripts.js"></script>
-  </body>
-</html>
+    
---
+++ templates/footer.php
@@ -0,0 +1,69 @@
+<footer class="footer-sitio pt-3 mt-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h3 class="text-uppercase text-center pb-4">Nosotros</h3>
+            <p class="text-justify">
+              Somos una empresa que nos caracterizamos por nuestras prendas de
+              alta calidad. Contamos con una alta gama de joyas que van desde
+              anillos de propuesta hasta pequeños broches.
+            </p>
+          </div>
+          <div class="col-md-4 text-center">
+            <h3 class="text-uppercase pb-4">Paginas</h3>
+            <a class="nav-link btn-link text-dark" href="index.html">Inicio</a>
+            <a class="nav-link btn-link text-dark" href="servicios.html"
+              >Servicios</a
+            >
+            <a class="nav-link btn-link text-dark" href="contacto.html"
+              >Contácto</a
+            >
+          </div>
+          <div class="col-md-4 text-center">
+            <h3 class="text-uppercase pb-4">Visitános</h3>
+            <p>Local pricipal: Valera, CC Edivica</p>
+            <p>Planta baja, local 37</p>
+            <nav class="sociales text-center">
+              <ul>
+                <li>
+                  <a href="http://facebook.com">
+                    <span class="sr-only"> Facebook</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://twitter.com">
+                    <span class="sr-only"> Twitter</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://instagram.com">
+                    <span class="sr-only"> Instagram</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://pinterest.com">
+                    <span class="sr-only"> Pinterest</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://youtube.com">
+                    <span class="sr-only"> YouTube</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <hr class="w-100" />
+          <p class="text-center copyright w-100">
+            Nova Joyeria <span id="footer"></span>. Todos los derechos
+            reservados &copysr;
+          </p>
+        </div>
+      </div>
+    </footer>
+    <script src="js/jquery.slim.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
