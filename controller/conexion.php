5bd4c7b9cf83da9294fe3a507ea759e15c8cc176
Brian <blsvsanteliz@gmail.com>
2021-03-13 02:06:12 -0400

    fixed bug in the charset_utf8

--- controller/conexion.php
+++ controller/conexion.php
@@ -1,6 +1,7 @@
 <?php 
     try {
         $conexion = new mysqli("localhost", "root", "300900", "joyeria-db");
+        $conexion->set_charset("utf8");
     } catch (Exception $error) {
         echo $error->getMessage();
         exit;
