889febf2b960d8e8b2d54b01ccb764737b9b0470
Brian <blsvsanteliz@gmail.com>
2021-03-29 22:40:12 -0400

    Modified file to add connection with php

--- controller/conexion.php
+++ controller/conexion.php
@@ -1,9 +1,7 @@
 <?php 
-    try {
         $conexion = new mysqli("localhost", "root", "300900", "joyeria-db");
         $conexion->set_charset("utf8");
-    } catch (Exception $error) {
-        echo $error->getMessage();
-        exit;
-    }
+        if ($conexion->connect_error) {
+           die("Ocurrió un error conectandose en la base de datos, intente más tarde: " . $conexion->connect_error);
+        }
 ?>
